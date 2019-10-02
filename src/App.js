8241c6778b5599f0b8db49c331407c4aacf10745
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2019-10-02 12:28:02 +0700

    add examples to App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import Container from './common/Container';
 
 import UseStateExl1 from './useState/UseStateExl1';
 import UseStateExl2 from './useState/UseStateExl2';
+import UseCallbackExl1 from './useCallback/useCallbackExl1';
+import MemoExl1 from './memo/MemoExl1';
+import MemoExl2 from './memo/MemoExl2';
 
 const App = () => (
     <Container>
         {/* Simple useState */}
-        <UseStateExl1 />
+        {/*<UseStateExl1 />*/}
+
         {/*input useState*/}
-        <UseStateExl2 />
+        {/*<UseStateExl2 />*/}
+
+        {/* memo */}
+        {/*<MemoExl1 />*/}
+
+        {/* memo 2*/}
+        {/*<MemoExl2 />*/}
+
+        {/*input useCallback*/}
+        <UseCallbackExl1 />
     </Container>
 );
 
