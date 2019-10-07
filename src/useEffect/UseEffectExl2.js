8c7c5d177eeb9a4a06f6b9b50dd108e7bcfd3756
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2019-10-07 20:55:09 +0700

    add useEffect prod example

---
+++ src/useEffect/UseEffectExl2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Island from '../common/Island';
+
+const UseEffectExl2 = () => {
+  // TODO
+
+    return (
+        <Island title="Simple useEffect">
+
+        </Island>
+    );
+};
+
+export default UseEffectExl2;
