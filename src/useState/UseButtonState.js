e100a66a5aa050a164c6c9d6277254585890ab3f
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2019-10-16 20:14:43 +0700

    UseButtonState version for presentation

--- src/useState/UseButtonState.js
+++ src/useState/UseButtonState.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import Island from "../common/Island";
+import React, { useState } from 'react';
+import Island from '../common/Island';
 
 const UseButtonState = ({ initialCount = 0 }) => {
 
-  return (
-    <Island title="UseButtonState">
-      Count: {initialCount}
-      <button onClick={() => {}}>Reset</button>
-      <button onClick={() => {}}>+</button>
-      <button onClick={() => {}}>-</button>
-    </Island>
-  );
+    return (
+        <Island title="UseButtonState">
+            Count: {initialCount}
+
+            <button onClick={() => {}}>Reset</button>
+            <button onClick={() => {}}>+</button>
+            <button onClick={() => {}}>-</button>
+        </Island>
+    );
 };
 
 export default UseButtonState;
