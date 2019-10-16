b689dd43a47e7ace675fa939ac6116e2ad00a2c7
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2019-10-16 20:21:19 +0700

    UseInputCallback version for presentation

--- src/useCallback/UseInputCallback.js
+++ src/useCallback/UseInputCallback.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Island from '../common/Island';
 import Text from '../common/Text';
 import Input from '../common/Input';
